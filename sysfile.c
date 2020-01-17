b32e92089122490801cba37625819129678d101f
Vitaly Pavlenko <cxielamiko@gmail.com>
2020-01-16 20:19:46 -0500

    Make PC speaker output work

--- sysfile.c
+++ sysfile.c
@@ -73,6 +73,16 @@ sys_read(void)
   int n;
   char *p;
 
+  asm volatile("movb  $182,%al");
+  asm volatile("outb  %al,$0x43");
+  asm volatile("movw  %cx,%ax");
+  asm volatile("outb  %al,$0x42");
+  asm volatile("movb  %ah,%al");
+  asm volatile("outb  %al,$0x42");
+  asm volatile("inb $0x61,%al");
+  asm volatile("orb $0B00000011,%al");
+  asm volatile("outb  %al,$0x61");
+
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
   return fileread(f, p, n);
